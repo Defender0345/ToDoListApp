93bd5d7e8907023069df7511fad8531016a74684
Defender0345 <dylannel02@gmail.com>
2024-03-29 16:54:37 +0200

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ToDoListApp.Models;
+
+namespace ToDoListApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ToDoListApp.Models.ToDo> ToDo { get; set; } = default!;
+    }
+}
---
+++ Models/ToDo.cs
@@ -0,0 +1,14 @@
+﻿namespace ToDoListApp.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string ToDoTitle { get; set; }
+        public string ToDoDescription { get; set; }
+
+        public ToDo()
+        {
+            
+        }
+    }
+}
